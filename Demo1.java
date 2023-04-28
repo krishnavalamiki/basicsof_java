5f557a9ae562e4a938e9b0b723374bc8f1ec4aac
krishnavalamiki <talarikrishna964073@gmail.com>
2023-04-28 16:36:39 +0530

    Operators_INjava

--- Demo1.java
+++ Demo1.java
@@ -4,9 +4,10 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 	 // ternaory oparater...
-		int i = 6;
+		//int i = 7;    Print 20 odd number...
+		int i = 6;      // print 10 even number...
 		int result =0;
-		result = i%2==0 ? 10:20;
+		result = i%2==0 ? 10:20;       
 		System.out.println(result);
 	}
 
---
+++ Ternary_Operator.java
@@ -0,0 +1,15 @@
+package JDBC_Program;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+	 
+		
+		int i = 8;
+		boolean result ;
+		result = (i%2==0)? true:false;     // Ternary Operator...
+		System.out.println(result);
+
+	}
+
+}
